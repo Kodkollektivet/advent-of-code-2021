a1466c6fc6e38d788ebffb1f0a06f25c0b70488a
johan <jg223fp@student.lnu.se>
2021-12-01 17:28:21 +0100

    part_2 completed

--- python/johan_g/day1/day1_part2.py
+++ python/johan_g/day1/day1_part2.py
@@ -1,4 +1,4 @@
-#advent of code 2021 day1  par2  Johan g
+#advent of code 2021 day1  part2  Johan g
 import os
 path = "C:\\Users\jocke\OneDrive\Dokument\Knacka_kod\Phyton\Datateknik\\advent_of_code\\advent-of-code-2021\python\johan_g\day1\depths.txt"
 
@@ -12,16 +12,16 @@ except IOError as io:
 
 
 slidingDepths = []
-for depth in depths:
-  print(depths.index(depth))
-  """  if depths.index(depth) <= (len(depths) - 3):
-    depthPlusOne = depths[depths.index(depth) + 1]
-    depthPlusTwo = depths[depths.index(depth) + 2]
-    slidignDepth = depth + depthPlusOne + depthPlusTwo
-    slidingDepths.append(slidignDepth) """
+i = 0
+while i < len(depths) - 2:
+  depthPlusOne = depths[i + 1]
+  depthPlusTwo = depths[i + 2]
+  slidignDepth = depths[i] + depthPlusOne + depthPlusTwo
+  slidingDepths.append(slidignDepth)
+  i += 1  
 
 
-""" increases = 0
+increases = 0
 prevDepth = slidingDepths[0]
 
 for slidingDepth in slidingDepths:
@@ -29,4 +29,4 @@ for slidingDepth in slidingDepths:
     increases += 1
   prevDepth = slidingDepth
 
-print("Number of increases: ", increases) """
+print("Number of increases: ", increases)
