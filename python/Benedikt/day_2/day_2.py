4bc5fb22e83866047c43d4f08b416876d8de8341
ralof-x <benedikt_chmie@gmx.de>
2021-12-02 16:15:54 +0100

    Day 2 part 1 complete and working again after breaking changes.

--- python/Benedikt/day_2/day_2.py
+++ python/Benedikt/day_2/day_2.py
@@ -2,21 +2,30 @@ file = 'input'
 
 
 def read(in_file):
-    dic = {}
+    lst = []
     with open(in_file, 'r') as lines:
         for line in lines:
             comp_line = line.strip()
             comp_lst = comp_line.split()
-            if comp_lst[0] not in dic and comp_lst[0] != 'up':
-                dic[comp_lst[0]] = int(comp_lst[1])
-            elif comp_lst[0] == 'up':
-                dic['down'] -= int(comp_lst[1])
-            else:
-                dic[comp_lst[0]] += int(comp_lst[1])
-    # print(dic)
+            lst.append(comp_lst)
+        res_part1 = part1(lst)
+    return res_part1
+
+
+def part1(lst):
+    dic = {}
+    for i in lst:
+        if i[0] not in dic and i[0] != 'up':
+            dic[i[0]] = int(i[1])
+        elif i[0] == 'up':
+            dic['down'] -= int(i[1])
+        else:
+            dic[i[0]] += int(i[1])
     horizontal = dic['forward']
     depth = dic['down']
     return horizontal * depth
+
+
 
 
 def main(file):
