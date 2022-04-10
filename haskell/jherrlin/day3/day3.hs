63b3fd05c04dac95816d96f1bd37faad24fe5e46
John Herrlin <john.herrlin@fortnox.se>
2022-04-10 10:28:46 +0200

    Haskell jherrlin day3 problem 2

--- haskell/jherrlin/day3/day3.hs
+++ haskell/jherrlin/day3/day3.hs
@@ -1,6 +1,6 @@
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as IntMap
-import Data.List (maximumBy)
+import Data.List (maximumBy, transpose)
 import Data.Function (on)
 
 
@@ -31,10 +31,44 @@ binaryListInverse :: [Int] -> [Int]
 binaryListInverse = map (\x -> if x == 0 then 1 else 0)
 
 problem1 = do
-  content <- readFile "input.txt"
-  let matrix      = stringToMatrix content
+  fileContent <- readFile "input.txt"
+  -- fileContent <- readFile "test-input.txt"
+  let matrix      = stringToMatrix fileContent
       columnWidth = (pred . length . head) matrix
       gamma       = map fst [largesPairInMap $ frequencies $ map (\xs -> xs !! x) matrix | x <- [0..columnWidth]]
       epsilon     = binaryListInverse gamma
       sum         = listWithBinariesToDecimal gamma * listWithBinariesToDecimal epsilon
   print gamma
+
+column :: [[Int]] -> Int -> [Int]
+column matrix id = map (\row -> row !! id) matrix
+
+lookupResult :: Maybe Int -> Int
+lookupResult (Just x) = x
+lookupResult _ = error "Map does not contain that key!"
+
+type Idx = Int
+type Matrix = [[Int]]
+type Binary = [Int]
+type Pred = (Int -> Int -> Int)
+
+calculate :: Matrix -> Pred -> Binary -> Idx -> Binary
+calculate [lastRow] p b idx = reverse (drop idx b) ++ lastRow
+calculate   m p b idx =
+  let zeros      = lookupResult $ IntMap.lookup 0 $ frequencies $ column m idx
+      ones       = lookupResult $ IntMap.lookup 1 $ frequencies $ column m idx
+      mostCommon = p zeros ones
+      newMatrix  = filter (\row -> mostCommon == (row !! idx)) m
+      newIdx     = succ idx
+      binary     = mostCommon:b
+  in calculate newMatrix p binary newIdx
+
+problem2 = do
+  fileContent <- readFile "input.txt"
+  -- fileContent <- readFile "test-input.txt"
+  let matrix        = stringToMatrix fileContent
+      oxygen        = calculate matrix (\z o -> if z <= o then 1 else 0) [] 0
+      co2           = calculate matrix (\z o -> if z <= o then 0 else 1) [] 0
+      oxygenInDec   = listWithBinariesToDecimal oxygen
+      co2InDec      = listWithBinariesToDecimal co2
+  print (oxygenInDec * co2InDec)
